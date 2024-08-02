eeaffe4aea17dbf9ffb8b171e2a86001e6100ce0
1dhruvsingh <dhruvsinghsir@gmail.com>
2024-08-03 00:06:02 +0530

    Exp-3
    Stack new experiment

--- exp-3.cpp
+++ exp-3.cpp
@@ -1,47 +1,80 @@
-#include <iostream>
-#include <stack>
-#include <string>
+#include<iostream>
 using namespace std;
-int evaluate(string exp) {
-    stack<int> stack;
-    
-    for (char& c : exp) {
-        if (isdigit(c)) {
-            stack.push(c - '0');
-        } 
-        else {
-            int val1 = stack.top();
-            stack.pop();
-            int val2 = stack.top();
-            stack.pop();
-            
-            switch (c) {
-                case '+':
-                    stack.push(val2 + val1);
-                    break;
-                case '-':
-                    stack.push(val2 - val1);
-                    break;
-                case '*':
-                    stack.push(val2 * val1);
-                    break;
-                case '/':
-                    stack.push(val2 / val1);
-                    break;
-            }
-        }
-    }
-    
-    return stack.top();
+int num;
+int top=-1;
+int push (int num)
+{
+if (top>=5)
+{
+cout<<"overflow"<<endl;
 }
+top=top+1;
+s[top]=num;
+}
+int pop()
+{
+if (top<0)
+{
+cout<<"underflow"<<endl;
+}
+num=s(top);
+top-top-1;
+return num;
+}
+int evaluate (char* expression)
+int i=0;
+char symbol=expression[i];
+int op1, op2, result;
+while (symbol!= '\0')
+{
+if (symbol>='0' && symbol<='9')
+{
+int num-symbol-'0';
+push (num);
+}
+else if (symbol)
+{
+op2=pop();
+op1=pop();
+switch (symbol)
+{
+case '+':
+{
+result=op1+op2;
+break;
+}
+case '-':
+{
+result=op1-op2;
+break;
+}
+case '*':
+{
+result=op1*op2;
 
-int main() {
-    string exp;
-    cout << "Enter a postfix expression: ";
-    cin >> exp;
-    
-    int result = evaluate(exp);
-    cout << "Result: " << result << endl;
-    
-    return 0;
+case '-':
+{ result=opl-op2; break;
+}
+case '*': {
+result=op1*op2; break;
+}
+case '/':
+{
+result=opl/op2;
+break;
+}
+push (result);
+}
+i++;
+}
+symbol=expression [i];
+}
+result=pop();
+return result;
+}
+int main(){
+char expression [] = "5 6 7 + * 8";
+int result = evaluate (expression);
+cout<<result<<endl;
+return 0;
 }
---
+++ exp3b.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+int precedence(char op) {
+    if (op == '+' || op == '-') {
+        return 1;
+    } else if (op == '*' || op == '/') {
+        return 2;
+    }
+    return 0;
+}
+
+std::string infixToPostfix(const std::string& infix) {
+    std::string postfix = "";
+    std::stack<char> operators;
+
+    for (char c : infix) {
+        if (isalnum(c)) {
+            postfix += c;
+        } else if (c == '(') {
+            operators.push(c);
+        } else if (c == ')') {
+            while (!operators.empty() && operators.top() != '(') {
+                postfix += operators.top();
+                operators.pop();
+            }
+            operators.pop();
+        } else {
+            while (!operators.empty() && precedence(c) <= precedence(operators.top())) {
+                postfix += operators.top();
+                operators.pop();
+            }
+            operators.push(c);
+        }
+    }
+
+    while (!operators.empty()) {
+        postfix += operators.top();
+        operators.pop();
+    }
+
+    return postfix;
+}
+
+int main() {
+    std::string infixExpression = "a+b*c-(d/e+f)*g";
+    std::string postfixExpression = infixToPostfix(infixExpression);
+    
+    std::cout << "Infix Expression: " << infixExpression << std::endl;
+    std::cout << "Postfix Expression: " << postfixExpression << std::endl;
+
+    return 0;
+}
