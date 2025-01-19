e28fc36c272cad5ba6af8a4a1b55ac37b64e5ac9
1dhruvsingh <dhruvsinghsir@gmail.com>
2025-01-19 06:32:38 +0530

    new

---
+++ Java/Demo.java
@@ -0,0 +1,9 @@
+class Demo {
+    public static void main(String arg[])
+    {
+        int a=5;
+        int b=10;
+        int sum = a+b;
+        System.out.println("addition of "+a+" and "+b+" = "+sum);
+    }
+}
