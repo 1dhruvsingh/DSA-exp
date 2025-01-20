1bd7705574c1974ec885a3790fb6e324d0ea0d87
1dhruvsingh <dhruvsinghsir@gmail.com>
2025-01-20 18:56:50 +0530

    Exp-1

---
+++ Java/Exp1.java
@@ -0,0 +1,46 @@
+class Exp1 {
+    public static void main(String args[]) {
+        int integerVar = 10;
+        double doubleVar = 20.5;
+        char charVar = 'A';
+        boolean booleanVar = true;
+        
+        System.out.println("Integer: " + integerVar);
+        System.out.println("Double: " + doubleVar);
+        System.out.println("Character: " + charVar);
+        System.out.println("Boolean: " + booleanVar);
+
+        int age = 25;
+        String name = "John Doe";
+        double salary = 50000.50;
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+
+        
+        int a = 10, b = 5;
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+        
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+
+        final double PI = 3.14159; 
+        double radius = 5.0;
+        double area = PI * radius * radius; 
+
+        System.out.println("Area of circle: " + area);
+
+        int validIdentifier = 1;
+        String anotherValidIdentifier = "Hello";
+        
+        System.out.println("Valid Identifier: " + validIdentifier);
+        System.out.println("Another Valid Identifier: " + anotherValidIdentifier);
+        
+    }
+}
